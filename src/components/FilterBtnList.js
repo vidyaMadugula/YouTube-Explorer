23a856016121e9d063fb37aeff1ab37956f44693
vidyaMadugula <vidyamadugula692@gmail.com>
2024-04-04 16:30:29 +0530

    first commit

---
+++ src/components/FilterBtnList.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link, useSearchParams } from "react-router-dom";
+// import { MdExplore } from "react-icons/md";
+
+const FilterBtnList = () => {
+  const [searchParams] = useSearchParams();
+  const filter = searchParams.get("filter");
+  const list = ["All", "HTML", "CSS", "Javascript", "ReactJS"];
+  const mdList = [
+    "NextJs",
+    "Live",
+    "Laptops",
+    "Music",
+    "Disney",
+    "Namaste JS",
+  ];
+
+  const buttonList = (list, style) =>
+    list.map((btn, i) => (
+      <Link key={i} to={"/?filter=" + btn}>
+        <button
+          className={`${
+            filter === btn ? "bg-gray-800 text-gray-50" : "bg-gray-100"
+          } ${style} `}
+        >
+          {btn}
+        </button>
+      </Link>
+    ));
+
+  const style = "px-5 py-3 m-2 rounded-lg";
+  const mdStyle = "px-5 py-3 m-2 rounded-lg hidden md:block";
+
+  return (
+    <div>
+      <div className="flex items-center border-b-2 md:border-none md:justify-center">
+        {/*  <Link to={"/"}>
+          <button className="flex items-center bg-gray-100 p-1 m-1 rounded-lg">
+            <MdExplore /> Explore
+          </button>
+        </Link>*/}
+        <span className="border-r-2 border-gray-200 h-6 "></span>
+        {buttonList(list, style)}
+        {buttonList(mdList, mdStyle)}
+      </div>
+    </div>
+  );
+};
+
+export default FilterBtnList;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,107 +1,48 @@
-// //const YOUTUBE_API_KEY = "AIzaSyBOT7nKw9Sz6GX7D72SXajQsz6gird8Yzc";
-// const YOUTUBE_API_KEY = "AIzaSyBBtjndDovAvLboXUYmTTdzU-SK_7ONJIg";
-// // export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&chart=mostPopular&maxResults=50&key=" + YOUTUBE_API_KEY
 
-// // using .env variable
-// export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&chart=mostPopular&maxResults=50&key=" +YOUTUBE_API_KEY
+// export const YOUTUBE_SEARCH_SUGGESTION_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+// const GOOGLE_API_KEY = "AIzaSyAIWnqZCBkhdUDqHs47v-6tBRmGWDQBu3k";
 
-export const YOUTUBE_SEARCH_SUGGESTION_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+// export const OFFSET_LIVE_CHAT = 25;
 
-// export const YOUTUBE_SEARCH_API = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=" +YOUTUBE_API_KEY
-// // Live Chat >>>> Infinite Scroll >>>>>> Pagination
+// export const YOUTUBE_VIDEOS_API =
+//   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
+//   GOOGLE_API_KEY;
 
-// export const LIVE_CHAT_COUNT = 25
-// // export const OFFSET_LIVE_CHAT = 25
-// //https://jsonplaceholder.typicode.com/posts
-// //http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=
+// export const YOUTUBE_SEARCH_API =
+//   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
-// export const commentData = [
-//   {
-//     name: "Ram Sharma",
-//     text: "Lorem ipsum dolor sit amet, consectetur adip",
-//     replies: []
-//   },
-//   {
-//     name: "Ram Sharma",
-//     text: "Lorem ipsum dolor sit amet, consectetur adip",
-//     replies: [
-//       {
-//         name: "Ram Sharma",
-//         text: "Lorem ipsum dolor sit amet, consectetur adip",
-//         replies: []
-//       },
-//       {
-//         name: "Ram Sharma",
-//         text: "Lorem ipsum dolor sit amet, consectetur adip",
-//         replies: [
-//           {
-//             name: "Ram Sharma",
-//             text: "Lorem ipsum dolor sit amet, consectetur adip",
-//             replies: [
-//               {
-//                 name: "Ram Sharma",
-//                 text: "Lorem ipsum dolor sit amet, consectetur adip",
-//                 replies: [
-//                   {
-//                     name: "Ram Sharma",
-//                     text: "Lorem ipsum dolor sit amet, consectetur adip",
-//                     replies: [
-//                       {
-//                         name: "Ram Sharma",
-//                         text: "Lorem ipsum dolor sit amet, consectetur adip",
-//                         replies: []
-//                       }
-//                     ]
-//                   },
-//                   {
-//                     name: "Ram Sharma",
-//                     text: "Lorem ipsum dolor sit amet, consectetur adip",
-//                     replies: []
-//                   }
-//                 ]
-//               }
-//             ]
-//           }
-//         ]
-//       }
-//     ]
-//   },
-//   {
-//     name: "Ram Sharma",
-//     text: "Lorem ipsum dolor sit amet, consectetur adip",
-//     replies: []
-//   },
-//   {
-//     name: "Ram Sharma",
-//     text: "Lorem ipsum dolor sit amet, consectetur adip",
-//     replies: []
-//   },
-//   {
-//     name: "Ram Sharma",
-//     text: "Lorem ipsum dolor sit amet, consectetur adip",
-//     replies: []
-//   },
-//   {
-//     name: "Ram Sharma",
-//     text: "Lorem ipsum dolor sit amet, consectetur adip",
-//     replies: []
-//   }
-// ]
+// 1.const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+const GOOGLE_API_KEY = "AIzaSyAIi8VgLmgWhKlLLLRkRAGWCco6Nj2nY_I";
 
-const GOOGLE_API_KEY = "AIzaSyAIWnqZCBkhdUDqHs47v-6tBRmGWDQBu3k";
-
-export const OFFSET_LIVE_CHAT = 25;
-
-export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
+export const YOUTUBE_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=48&regionCode=IN&key=" +
   GOOGLE_API_KEY;
 
-export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const VIDEO_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=" +
+  GOOGLE_API_KEY +
+  "&id=";
 
-  // "predeploy": "npm run build",
-  // "deploy": "gh-pages -d build",
-  // "homepage": "https://vidyaMadugula.github.io/youtube-explorer",
+export const YOUTUBE_SEARCH_API =
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&key=" +
+  GOOGLE_API_KEY +
+  "&q=";
+
+export const SEARCH_RESULT_API =
+  "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=" +
+  GOOGLE_API_KEY +
+  "&q=";
+
+export const COMMENTS_API =
+  "https://www.googleapis.com/youtube/v3/commentThreads?textFormat=plainText&part=snippet&maxResults=30&key=" +
+  GOOGLE_API_KEY +
+  "&videoId=";
+
+export const kFormatter = (num) => {
+  return Math.abs(num) > 999
+    ? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + "k"
+    : Math.sign(num) * Math.abs(num);
+};
 
 
 
